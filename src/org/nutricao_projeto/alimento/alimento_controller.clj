23cc2f087a55e255d6df869890021fb2ab0a0d98
DaviSilveira16 <158481047+DaviSilveira16@users.noreply.github.com>
2025-06-01 12:44:30 -0300

    Update alimento_controller.clj

--- src/org/nutricao_projeto/alimento/alimento_controller.clj
+++ src/org/nutricao_projeto/alimento/alimento_controller.clj
@@ -2,7 +2,14 @@
   (:require [cheshire.core :as json]
             [clj-http.client :as client]
             [clojure.string :as str]
-            [org.nutricao-projeto.traducao.traduzir-frase :as trad]))
+            [org.nutricao-projeto.traducao.traduzir-frase :as trad])
+  (:import [java.time LocalDateTime]
+           [java.time.format DateTimeFormatter]))
+
+(defn agora-formatado []
+  (let [agora (LocalDateTime/now)
+        formatador (DateTimeFormatter/ofPattern "yyyy-MM-dd HH:mm:ss")]
+    (.format agora formatador)))
 
 (defn buscar-alimento [nome]
   (let [url (str "http://localhost:3000/alimento/" nome)
@@ -26,23 +33,29 @@
         (recur (rest nutrientes) nome)))))
 
 (defn imprimir-refeicoes [refeicoes]
-  (println "---------------------------------------------------------")
-  (println (format "%-25s %8s %10s %10s %10s %14s"
-                   "Alimento" "Qtd(g)" "Calorias" "Proteína" "Gordura" "Carboidrato"))
-  (println "---------------------------------------------------------")
-  (letfn [(imprimir-rec [lst]
+  (println "----------------------------------------------------------------------------------------------------------")
+  (println (format "%-25s %8s %10s %10s %10s %14s %20s"
+                   "Alimento" "Qtd(g)" "Calorias" "Proteína" "Gordura" "Carboidrato" "Data/Hora"))
+  (println "----------------------------------------------------------------------------------------------------------")
+  (letfn [(imprimir-rec [lst acc]
             (if (empty? lst)
-              (println "---------------------------------------------------------")
-              (let [r (first lst)]
-                (println (format "%-25s %8.1f %10.2f %10.2f %10.2f %14.2f"
-                                 (:alimento r)
-                                 (:quantidade r)
-                                 (:calorias r)
-                                 (:proteina r)
-                                 (:gordura r)
-                                 (:carboidrato r)))
-                (recur (rest lst)))))]
-    (imprimir-rec refeicoes)))
+              (do
+                (println "----------------------------------------------------------------------------------------------------------")
+                (println (format "Total de calorias consumidas no dia: %.2f kcal" acc)))
+              (let [r (first lst)
+                    cal (:calorias r)]
+                (do
+                  (println (format "%-25s %8.1f %10.2f %10.2f %10.2f %14.2f %20s"
+                                   (:alimento r)
+                                   (:quantidade r)
+                                   (:calorias r)
+                                   (:proteina r)
+                                   (:gordura r)
+                                   (:carboidrato r)
+                                   (:data-hora r)))
+                  (recur (rest lst) (+ acc cal))))))
+          ]
+    (imprimir-rec refeicoes 0.0)))
 
 (defn inteiro? [s]
   (boolean (re-matches #"\d+" s)))
@@ -50,17 +63,13 @@
 (defn double-str? [s]
   (boolean (re-matches #"^\d+(\.\d+)?$" s)))
 
-(defn imprimir-alimentos-indexados [alimentos idx]
-  (if (empty? alimentos)
+(defn imprimir-opcoes [traduzidos i]
+  (if (>= i (count traduzidos))
     nil
-    (let [a (first alimentos)
-          i idx
-          linha (str (inc i) ". "
-                     (try
-                       (trad/ingles-portugues (:descricao-detalhada a))
-                       (catch Exception _ (:descricao-detalhada a))))]
-      (println linha)
-      (recur (rest alimentos) (inc i)))))
+    (do
+      (let [alimento (nth traduzidos i)]
+        (println (str (+ i 1) ". " (:descricao-pt alimento))))
+      (recur traduzidos (+ i 1)))))
 
 (defn adicionar-refeicao [refeicoes]
   (println "Digite os alimentos consumidos separados por vírgula (ex: arroz, feijao, ovo):")
@@ -72,10 +81,11 @@
                 (let [nome (first alimentos)
                       resultado (buscar-alimento nome)
                       alimentos-filtrados (take 5 resultado)
-                      traduzidos (mapv #(assoc % :descricao-pt
-                                                 (try
-                                                   (trad/ingles-portugues (:descricao-detalhada %))
-                                                   (catch Exception _ (:descricao-detalhada %))))
+                      traduzidos (mapv (fn [a]
+                                         (assoc a :descricao-pt
+                                                  (try
+                                                    (trad/ingles-portugues (:descricao-detalhada a))
+                                                    (catch Exception _ (:descricao-detalhada a)))))
                                        alimentos-filtrados)]
                   (if (empty? traduzidos)
                     (do
@@ -83,7 +93,7 @@
                       (recur (rest alimentos) acumulado))
                     (do
                       (println "\nSelecione o alimento correspondente:")
-                      (imprimir-alimentos-indexados traduzidos 0)
+                      (imprimir-opcoes traduzidos 0)
                       (let [opcao-str (read-line)
                             opcao (try (Integer/parseInt opcao-str) (catch Exception _ 0))]
                         (if (or (< opcao 1) (> opcao (count traduzidos)))
@@ -107,7 +117,8 @@
                                                      :calorias (* cal-100g fator)
                                                      :proteina (* prot-100g fator)
                                                      :gordura  (* gord-100g fator)
-                                                     :carboidrato (* carb-100g fator)}]
+                                                     :carboidrato (* carb-100g fator)
+                                                     :data-hora (agora-formatado)}]
                                   (println (str "Adicionado: " descricao " (" qtd "g)"))
                                   (enviar-refeicao nova-refeicao)
                                   (recur (rest alimentos) (conj acumulado nova-refeicao)))
@@ -120,9 +131,7 @@
   (if (or (empty? alimentos) (>= count 5))
     nil
     (let [alimento (first alimentos)
-          descricao (try
-                      (trad/ingles-portugues (:descricao-detalhada alimento))
-                      (catch Exception _ (:descricao-detalhada alimento)))
+          descricao (:descricao-pt alimento)
           categoria (:foodCategory alimento)
           nutrientes (:foodNutrients alimento)
           calorias (obter-nutriente nutrientes "Energy")
